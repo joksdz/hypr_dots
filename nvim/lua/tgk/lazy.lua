527db7cda24da622844554894a4a48f69b465691
joksdz <joksdzjojo2007@gmail.com>
2024-03-11 14:35:14 +0100

    hmmm

--- nvim/lua/tgk/lazy.lua
+++ nvim/lua/tgk/lazy.lua
@@ -80,12 +80,7 @@ require("lazy").setup({
     vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
     vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
     vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
-
-
-
-
-			
-		end		
+end
 
 
 	},
