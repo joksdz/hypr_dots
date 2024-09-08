de7cb05ae6af0d4ea862a55e51dd9fc6087ac1f5
joksdz <joksdzjojo2007@gmail.com>
2024-09-08 22:01:33 +0100

    added colors to the line number since it was impossible to read with most backgrounds

--- nvim/lua/tgk/set.lua
+++ nvim/lua/tgk/set.lua
@@ -3,6 +3,9 @@ vim.o.relativenumber = true
 vim.cmd[[colorscheme tokyonight]]
 vim.o.hlsearch = false
 vim.o.incsearch=true
+vim.cmd[[highlight LineNr guifg=#ff0000]]
+vim.cmd[[highlight LineNrAbove guifg=#c2c2c2]]
+vim.cmd[[highlight LineNrBelow guifg=#c2c2c2]]
 vim.o.scrolloff= 8
 vim.o.updatetime = 50
 
