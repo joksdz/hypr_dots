ee0e9b1057fc5519f136ecccf860133e3257e655
joksdz <joksdzjojo2007@gmail.com>
2024-12-21 11:34:43 +0100

    comments and copying files , sddm setup

--- install.sh
+++ install.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
+# updates
 pacman -Syu
 
-
+cd .
 current=$(pwd)
 
 read -p "Enter your username: " user
-
+# check for ~/.config
 if  [ -e "/home/$user/.config" ]; then
   
   conf_f="/home/$user/.config"
   cd $conf_f
   
-
+#installing yay
   sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
- 
-yay -S swaylock kitty waybar wofi wlogout neovim thunar fish pulseaudio pipewire alsamixer nmtui speedtest-cli anime-cli mpv btop swww ffmpeg hyprshot sddm blueman firefox nwg-look qt6
+# installing main packeges  
+yay -S swaylock kitty waybar wofi wlogout neovim thunar fish pulseaudio pipewire alsamixer nmtui speedtest-cli anime-cli mpv btop swww ffmpeg hyprshot sddm blueman firefox nwg-look qt6 qt6-svg bc
 
-  ls > files.txt
+# setup for sddm 
+git clone https://github.com/keyitdev/sddm-astronaut-theme.git /usr/share/sddm/themes/sddm-astronaut-theme
+sudo cp /usr/share/sddm/themes/sddm-astronaut-theme/Fonts/* /usr/share/fonts/
+
+echo "[Theme]
+Current=sddm-astronaut-theme" | sudo tee /etc/sddm.conf
+# copying files and makes them into an arry  
+ls > files.txt
   files=($(cat files.txt))
-
+# checks if directory already exists in ~/.config
   for i in "${files[@]}"; do
 	if [  "$i" == "hypr" ] || [ "$i" == "nvim" ]|| [ "$i" == "swaylock" ] || [ "$i" == "waybar" ] || [ "$i" == "wofi" ] || [ "$i" == "kitty" ] || [ "$i" == "wlogout" ]  ; then
-		echo "exits"
+		# removes the directory
+		rm -rf $i
 	fi
+	# copys the filles from the repo to the ~/.config
+cp -r $current/$i /home/$user/.config
+
 	done
 
+# if /.config doesn't make one and do the rest  
 else
   
 	mkdir /home/$user/.config
