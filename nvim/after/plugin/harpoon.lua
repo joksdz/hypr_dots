e620ca25c8537de3e64efbf1a7086c23d217b3f7
joksdz <joksdzjojo2007@gmail.com>
2025-07-05 23:03:53 +0100

    changed harpoon shortcuts

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -4,13 +4,13 @@ local harpoon = require("harpoon")
 harpoon:setup()
 -- REQUIRED
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-c>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-,>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<C-d>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-f>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-x>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-t>", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
