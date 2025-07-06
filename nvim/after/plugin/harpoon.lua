02e24b8d6bd78951e807cf22e463823df1311cff
joksdz <joksdzjojo2007@gmail.com>
2025-07-06 14:22:01 +0100

    changed harpoon again cuz am a retard

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -7,7 +7,7 @@ harpoon:setup()
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-d>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-s>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<C-f>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<C-x>", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<C-t>", function() harpoon:list():select(4) end)
